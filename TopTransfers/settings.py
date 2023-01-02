045c7c8fb3bf9698ca746a12a28d9613a98d49bf
fismoilov20 <fismoilov20@gmail.com>
2023-01-03 01:00:53 +0500

    updated

--- TopTransfers/settings.py
+++ TopTransfers/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure--u1v)_swx7ifgot7lwcu)_-%lnr-!7m(*%pbwq(1q4=jr$+tcd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
