34446542084376afeffb0519c328e00696aadfe4
fismoilov20 <fismoilov20@gmail.com>
2023-01-03 22:38:12 +0500

    updated

--- TopTransfers/settings.py
+++ TopTransfers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--u1v)_swx7ifgot7lwcu)_-%lnr-!7m(*%pbwq(1q4=jr$+tcd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
